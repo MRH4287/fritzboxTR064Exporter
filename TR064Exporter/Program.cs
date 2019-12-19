ee0580816bfd2d5bf548845bbfc383de8d3a8b25
Manuel Holzhauser <Manuel@mrh-development.de>
2019-12-19 19:38:49 +0100

    Added Dockerfile

--- TR064Exporter/Program.cs
+++ TR064Exporter/Program.cs
@@ -20,6 +20,7 @@ namespace TR064Exporter
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000")
                 .UseEnvironment("Development");
 
     }
